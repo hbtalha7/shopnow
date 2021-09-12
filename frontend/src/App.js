f396b4535b3859fc87f8303103f57c7523a0cc3b
MOHAMMED TALHA <atalhahb@gmail.com>
2021-09-12 21:05:13 +0530

    better

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import react from "react";
-import { BrowserRouter ,Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./index.css";
 import Additemscreen from "./screens/Additemscreen";
@@ -10,18 +10,23 @@ import Loginscreen from "./screens/Loginscreen";
 function App() {
   return (
     <div className="container">
-    <BrowserRouter>
-      <header>
-        <Navbar></Navbar>
-      </header>
-      <main>
-      <Route path="/item/:id" component={Itemscreen}></Route>
-      <Route path="/login/" component={Loginscreen}></Route>
-      <Route path="/additem/" component={Additemscreen}></Route>
-        <Route exact path="/" component={Homescreen}></Route>
-      </main>
-      <footer></footer>
-    </BrowserRouter>
+      <BrowserRouter>
+        <header>
+          <Navbar></Navbar>
+        </header>
+        <main>
+          <Route path="/item/:id" component={Itemscreen}></Route>
+          <Route path="/login/" component={Loginscreen}></Route>
+          <Route path="/additem/" component={Additemscreen}></Route>
+          <Route exact path="/" component={Homescreen}></Route>
+        </main>
+        <footer>
+          <p>Create By:Talha</p>
+          <p>
+            <a href="github.com/hbtalha7">Github</a>
+          </p>
+        </footer>
+      </BrowserRouter>
     </div>
   );
 }
